e4b33f4e7bde42625c4b2793daf10f3d5ea2ddfe
jainabhishek02 <jainabhishek0211@gmail.com>
2015-12-31 03:04:55 +0530

    NO javascript error

--- js/agency.js
+++ js/agency.js
@@ -3,9 +3,6 @@
  * Code licensed under the Apache License v2.0.
  * For details, see http://www.apache.org/licenses/LICENSE-2.0.
  */
-$(document).ready(function(){
-  $('.bxslider').bxSlider();
-});
 // jQuery for page scrolling feature - requires jQuery Easing plugin
 $(function() {
     $('a.page-scroll').bind('click', function(event) {
